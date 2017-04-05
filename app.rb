a125ae436cc38312f76b01cf4c476c4d7a19130c
Heather Frantisak <hfrantisak@mvnp.com>
2017-04-05 09:52:54 -1000

    Set array for words to be an instance variable

--- app.rb
+++ app.rb
@@ -2,6 +2,11 @@
 # for words in a given corpus (document).
 class TermFrequency
 
+  def initialize
+    # Store all words in a corpus
+    @words_array = Array.new
+  end
+
   # Find all files in a given directory
   def get_files(file_path)
     Dir.foreach(file_path) do |file|
@@ -15,17 +20,16 @@ class TermFrequency
   # First iterate through each line, then by each word.
   # Words are stored in an array.
   def read_contents(file_path, text_file)
-    words_array = Array.new
     File.open("#{file_path}/#{text_file}", 'r') do |chapter|
       chapter.each_line do |line|
         split_content = line.downcase.gsub(/[^0-9a-z]/, ' ').split(' ')
         split_content.each do |word|
-          words_array.push(word)
+          @words_array.push(word)
         end
       end
     end
-    puts words_array.length
-    return words_array
+    puts @words_array.length
+    return @words_array
   end
 
 end
